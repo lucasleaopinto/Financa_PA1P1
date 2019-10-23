d009b783cbd4b3d4397aee559544d99ee20cc897
lucasleaopinto <lucasleaop@gmail.com>
2019-10-23 18:38:15 -0300

    Primeiro Commit após reutilizar o Layout

---
+++ app/src/main/java/br/edu/isfp/financas_pa1p1/view/ListaTransacoesActivity.kt
@@ -0,0 +1,16 @@
+package br.edu.isfp.financas_pa1p1.view
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.isfp.financas_pa1p1.R
+
+class ListaTransacoesActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+
+    }
+
+}
